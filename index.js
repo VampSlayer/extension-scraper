4d361608d5776192c020483e5944184b4dd1a2f0
VampSlayer <sayam+git@momentumdash.com>
2021-10-11 16:23:54 +0100

    use different package

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
-const createHandler = require("azure-function-express").createHandler
-const express = require("express")
-
-// Create express app as usual
-const app = express()
-app.get("/api/versions", (req, res) => {
-	res.json({
-		success: true,
-	})
-})
-
-// Binds the express app to an Azure Function handler
-module.exports = createHandler(app)
+const express = require('express');
+const azureFunctionHandler = require('azure-aws-serverless-express');
+ 
+const app = express();
+ 
+app.get('/api/hello-world/', (req, res) => res.send('Hello World!'));
+ 
+module.exports = azureFunctionHandler(app);
