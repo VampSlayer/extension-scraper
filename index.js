2969a5ff6008dcc32c52489df5f427eee4f7cf30
VampSlayer <sayam+git@momentumdash.com>
2021-10-11 19:20:48 +0100

    Add Dockerfile and parallel scraping

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
 const puppeteer = require("puppeteer")
-const timeago = require("timeago.js")
+const format = require("timeago.js").format
 const express = require("express")
 const NodeCache = require("node-cache")
 
@@ -12,7 +12,7 @@ app.get("/", async function (req, res) {
 	let versions = versionCache.get(versionCacheKey)
 	if (versions === undefined) {
 		versions = await getExtensionVersions()
-		const succces = versionCache.set(versionCacheKey, versions, 1800) // 30 minutes
+		const succces = versionCache.set(versionCacheKey, versions, 30 * 60) // 30 minutes
 		versions.cached = succces
 	}
 	res.send(versions)
@@ -23,144 +23,151 @@ console.log(`running @ localhost:${PORT}`)
 
 async function getExtensionVersions() {
 	const browser = await puppeteer.launch({ args: ['--no-sandbox'] }) // heroku build pack fix
-	let chrome
-	let firefox
-	let edge
-	let safari
 
-	try {
-		chrome = await getCurrentChromeVersion(browser)
-	} catch (error) {
-		console.error(error)
-		chrome = 'error'
-	}
+	let chrome = getCurrentChromeVersion(browser)
+	let firefox = getCurrentFirefoxVersion(browser)
+	let edge = getCurrentEdgeVersion(browser)
+	let safari = getCurrentSafariVersion(browser)
 
-	try {
-		firefox = await getCurrentFirefoxVersion(browser)
-	} catch (error) {
-		console.error(error)
-		firefox = 'error'
-	}
-
-	try {
-		edge = await getCurrentEdgeVersion(browser)
-	} catch (error) {
-		console.error(error)
-		edge = 'error'
-	}
-
-	try {
-		safari = await getCurrentSafariVersion(browser)
-	} catch (error) {
-		console.error(error)
-		safari = 'error'
-	}
+	let scraped_versions = await Promise.all([chrome, firefox, edge, safari]);
 
 	const months = ["Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"];
 	const today = new Date()
 	
 	const versions = {
 		date: `${months[today.getMonth()]} ${today.getDate()}, ${today.getFullYear()}`,
-		chrome: chrome,
-		firefox: firefox,
-		edge: edge,
-		safari: safari
+		chrome: scraped_versions[0],
+		firefox: scraped_versions[1],
+		edge: scraped_versions[2],
+		safari: scraped_versions[3],
 	}
+
 	await browser.close()
+
 	return versions
 }
 
 async function getCurrentChromeVersion(browser) {
-	const page = await browser.newPage()
-	await page.goto(
-		"https://chrome.google.com/webstore/detail/momentum/laookkfknpbbblfpciffpaejjkokdgca"
-	)
+	try {
+		const page = await browser.newPage()
+		await page.goto(
+			"https://chrome.google.com/webstore/detail/momentum/laookkfknpbbblfpciffpaejjkokdgca"
+		)
 
-	await page.waitFor(".h-C-b-p-D-md")
-	const version = await page.evaluate(() => {
-		const version_span = document.getElementsByClassName("h-C-b-p-D-md")[0]
-		return version_span.innerHTML
-	})
+		try {
+			await page.waitFor(".h-C-b-p-D-md")
+		} catch {
+			await page.waitFor(".DuMIQc")
+			await page.evaluate(() => {
+				const agreeButton = document.getElementsByClassName("DuMIQc")[3]
+				agreeButton.click()
+			})
+			await page.waitFor(".h-C-b-p-D-md")
+		}
 
-	await page.waitFor(".h-C-b-p-D-xh-hh")
-	const lastUpdated = await page.evaluate(() => {
-		const last_update_span = document.getElementsByClassName("h-C-b-p-D-xh-hh")[0]
-		return last_update_span.innerHTML
-	})
+		const version = await page.evaluate(() => {
+			const version_span = document.getElementsByClassName("h-C-b-p-D-md")[0]
+			return version_span.innerHTML
+		})
 
-	const timeAgo = timeago.format(lastUpdated)
+		await page.waitFor(".h-C-b-p-D-xh-hh")
+		const lastUpdated = await page.evaluate(() => {
+			const last_update_span = document.getElementsByClassName("h-C-b-p-D-xh-hh")[0]
+			return last_update_span.innerHTML
+		})
 
-	return { version, lastUpdated, timeAgo }
+		const timeAgo = format(lastUpdated)
+
+		return { version, lastUpdated, timeAgo }
+	} catch (error) {
+		console.log(error)
+		return "error"
+	}
 }
 
 async function getCurrentFirefoxVersion(browser) {
-	const page = await browser.newPage()
-	await page.goto(
-		"https://addons.mozilla.org/en-CA/firefox/addon/momentumdash/"
-	)
+	try {
+		const page = await browser.newPage()
+		await page.goto(
+			"https://addons.mozilla.org/en-CA/firefox/addon/momentumdash/"
+		)
 
-	await page.waitFor(".AddonMoreInfo-version")
-	const version = await page.evaluate(() => {
-		const version_span = document.getElementsByClassName(
-			"AddonMoreInfo-version"
-		)[0]
-		return version_span.innerHTML
-	})
+		await page.waitFor(".AddonMoreInfo-version")
+		const version = await page.evaluate(() => {
+			const version_span = document.getElementsByClassName(
+				"AddonMoreInfo-version"
+			)[0]
+			return version_span.innerHTML
+		})
 
-	await page.waitFor(".AddonMoreInfo-last-updated")
-	const lastUpdated = await page.evaluate(() => {
-		const last_updated = document.getElementsByClassName(
-			"AddonMoreInfo-last-updated"
-		)[0]
-		const inner_html = last_updated.innerHTML
-		return inner_html.substring(inner_html.lastIndexOf("(") + 1, inner_html.lastIndexOf(")"))
-	})
+		await page.waitFor(".AddonMoreInfo-last-updated")
+		const lastUpdated = await page.evaluate(() => {
+			const last_updated = document.getElementsByClassName(
+				"AddonMoreInfo-last-updated"
+			)[0]
+			const inner_html = last_updated.innerHTML
+			return inner_html.substring(inner_html.lastIndexOf("(") + 1, inner_html.lastIndexOf(")"))
+		})
 
-	const timeAgo = timeago.format(lastUpdated)
+		const timeAgo = format(lastUpdated)
 
-	return { version, lastUpdated, timeAgo }
+		return { version, lastUpdated, timeAgo }
+	} catch (error) {
+		console.log(error)
+		return "error"
+	}
 }
 
 async function getCurrentEdgeVersion(browser) {
-	const page = await browser.newPage()
-	await page.goto(
-		"https://microsoftedge.microsoft.com/addons/detail/momentum/jdoanlopeanabgejgmdncljhkdplcfed"
-	)
+	try {
+		const page = await browser.newPage()
+		await page.goto(
+			"https://microsoftedge.microsoft.com/addons/detail/momentum/jdoanlopeanabgejgmdncljhkdplcfed"
+		)
 
-	await page.waitForSelector("#versionLabel")
-	const version = await page.evaluate(() => {
-		const version_span = document.getElementById("versionLabel")
-		return version_span.innerHTML.split(' ')[1]
-	})
+		await page.waitForSelector("#versionLabel")
+		const version = await page.evaluate(() => {
+			const version_span = document.getElementById("versionLabel")
+			return version_span.innerHTML.split(' ')[1]
+		})
 
-	await page.waitForSelector("#lastUpdatedOnHeader")
-	const lastUpdated = await page.evaluate(() => {
-		const last_updated = document.getElementById("lastUpdatedOnHeader")
-		const last_updated_split = last_updated.innerHTML.split(' ')
-		return `${last_updated_split[1].substring(0, 3)} ${last_updated_split[2]} ${last_updated_split[3]}`
-	})
+		await page.waitForSelector("#lastUpdatedOnHeader")
+		const lastUpdated = await page.evaluate(() => {
+			const last_updated = document.getElementById("lastUpdatedOnHeader")
+			const last_updated_split = last_updated.innerHTML.split(' ')
+			return `${last_updated_split[1].substring(0, 3)} ${last_updated_split[2]} ${last_updated_split[3]}`
+		})
 
-	const timeAgo = timeago.format(lastUpdated)
+		const timeAgo = format(lastUpdated)
 
-	return { version, lastUpdated, timeAgo }
+		return { version, lastUpdated, timeAgo }
+	} catch (error) {
+		console.log(error)
+		return "error"
+	}
 }
 
 async function getCurrentSafariVersion(browser) {
-	const page = await browser.newPage()
-	await page.goto(
-		"https://apps.apple.com/ca/app/momentum/id1564329434"
-	)
+	try {
+		const page = await browser.newPage()
+		await page.goto(
+			"https://apps.apple.com/ca/app/momentum/id1564329434"
+		)
 
-	const version = await page.evaluate(() => {
-		const version_p = document.getElementsByClassName("whats-new__latest__version")[0]
-		return version_p.innerHTML.split(" ")[1]
-	})
+		const version = await page.evaluate(() => {
+			const version_p = document.getElementsByClassName("whats-new__latest__version")[0]
+			return version_p && version_p.innerHTML.split(" ")[1]
+		})
 
-	const lastUpdated = await page.evaluate(() => {
-		return document.querySelectorAll("[data-test-we-datetime]")[0].innerText.replace('.', '')
-	})
+		const lastUpdated = await page.evaluate(() => {
+			return document.querySelectorAll("[data-test-we-datetime]")[0].innerText.replace('.', '')
+		})
 
-	const timeAgo = timeago.format(lastUpdated)
+		const timeAgo = format(lastUpdated)
 
-	return { version, lastUpdated, timeAgo }
+		return { version, lastUpdated, timeAgo }
+	} catch (error) {
+		console.log(error)
+		return "error"
+	}
 }
